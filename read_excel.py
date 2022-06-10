d57fa1e5b115c366e61fb9318d1225d0a547147d
Greyjacket <asch716@gmail.com>
2022-06-09 21:27:51 -0400

    Pandas Dataframing

---
+++ read_excel.py
@@ -0,0 +1,19 @@
+import xlrd
+import pandas as pd
+
+file_errors_location = 'c2c.xlsx'
+df = pd.read_excel(file_errors_location)
+
+for row in df.iterrows():
+    print(row)
+    pass
+
+# Give the location of the file
+# loc = ("c2c.xlsx")
+ 
+# # To open Workbook
+# wb = xlrd.open_workbook(loc)
+# sheet = wb.sheet_by_index(0)
+ 
+# # For row 0 and column 0
+# print(sheet.cell_value(0, 0))
